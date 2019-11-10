0ed2311aaea4d83efed8d4e6ed230e1505fcdfde
Abdi <oxabdi75@gmail.com>
2019-11-10 11:34:49 +0300

    added a function with this.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function order(crust, topping, size, totalCost){
+    this.crust = crust;
+    this.topping = topping;
+    this.size = size;
+    this.number = number;
+    this.totalCost = totalCost;
+}
