217a8b9f5aa4ed67dfff857c1ab7c9fc32148bc9
Abdi <oxabdi75@gmail.com>
2019-11-10 11:54:53 +0300

    added .prototype and variables

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ function order(crust, topping, size, totalCost){
     this.number = number;
     this.totalCost = totalCost;
 }
+Order.prototype.fullOrder = function(){
+    return this.crust + " " + this.topping + " " + " " + this.size + " " + this.number + " " + this.totalCost;
+}
+document.getElementById("deli").addEventListener("click", function () {
+  prompt("enter where you are located");
+  alert("Your order will be delivered to your location for only 150ksh; Thank you!");
+ });
+$(document).ready(function() {
+  $("form#ordering").submit(function(event) {
+    event.preventDefault();
+
+    });
+$(".btn").click(function() {
+var crusts = $("select#crust").val();
+var toppings = $("select#topping").val();
+var sizes = $("select#size").val();
+var numbers =$("input#nbr").val();
