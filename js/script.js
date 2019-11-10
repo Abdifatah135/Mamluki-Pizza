2a95b0c8b1b926c98e539ab0fd72ce80e2b4a94e
Abdi <oxabdi75@gmail.com>
2019-11-10 11:59:46 +0300

    added a switch function to script.js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,24 @@ var crusts = $("select#crust").val();
 var toppings = $("select#topping").val();
 var sizes = $("select#size").val();
 var numbers =$("input#nbr").val();
+
+
+var cost, totalCost;
+     switch (sizes) {
+       case sizes = "large" :
+           cost = 1000;
+       if (crusts === "crispy") {
+           totalCost = cost + 150;
+       }
+      else if (crusts === "stuffed") {
+         totalCost = cost + 200 ;
+       }
+       else if (crusts === "gluten-free") {
+         totalCost = cost + 250;
+       }
+       break;
+       case sizes ="medium" :
+       cost = 800;
+       if (crusts === "crispy"){
+         totalCost = cost + 150;
+       }
